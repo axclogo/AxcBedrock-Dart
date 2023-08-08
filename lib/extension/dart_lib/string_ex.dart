c4c28fecea628c1144241ff9095f6370fad41d9e
axclogo <axclogo@163.com>
2023-08-08 15:40:46 +0800

    add: the string extension add 'isContainEmoji' function

--- lib/extension/dart_lib/string_ex.dart
+++ lib/extension/dart_lib/string_ex.dart
@@ -157,6 +157,19 @@ extension StringToApi on AxcStringSpace {
   }
 
   // MARK: ================================分隔操作================================
+  /// 返回一个新的子字符串，包含从[start]（包括）到[end]（包括）之间的所有字符。
+  /// 如果省略[end]，则会设置为`lastIndex`。
+  ///
+  /// ```dart
+  /// print('awesomeString'.axc.substring(0,6)); // awesome
+  /// print('awesomeString'.axc.substring(7)); // String
+  /// ```
+  String substring(int start, [int end = -1]) {
+    final _start = start < 0 ? start + base.length : start;
+    final _end = end < 0 ? end + base.length : end;
+    RangeError.checkValidRange(_start, _end, base.length);
+    return substring(_start, _end + 1);
+  }
 
   /// 通过AxcIntRange来获取一段子串
   String substringWithRange(AxcIntRange range) {
@@ -224,5 +237,70 @@ extension StringToApi on AxcStringSpace {
       }
     }
     return ranges;
+  }
+}
+
+/// 决策判断
+extension StringToGuard on AxcStringSpace {
+  /// 是否不为空
+  bool isNotEmpty() {
+    return !isEmpty();
+  }
+
+  /// 是否为空
+  bool isEmpty() {
+    return base.isEmpty;
+  }
+
+  /// 如果字符串是ASCII编码则返回' true '
+  bool get isAscii {
+    const _ascii = 0x007f;
+    for (final codeUnit in base.codeUnits) {
+      if (codeUnit > _ascii) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  /// 判断是否包含Emoji表情
+  bool hasEmoji(String text) {
+    /**
+     [\u{1F300}-\u{1F64F}]: This range matches emoji characters from the Miscellaneous Symbols and Pictographs block, which includes various symbols and pictographs.
+[\u{2702}-\u{27B0}]: This range matches emoji characters from the Dingbats block, which includes symbols and icons.
+[\u{1F680}-\u{1F6FF}]: This range matches emoji characters from the Transport and Map Symbols block, which includes symbols related to transportation, maps, and vehicles.
+[\u{2600}-\u{26FF}]: This range matches emoji characters from the Miscellaneous Symbols block, which includes various symbols and icons.
+[\u{1F1E6}-\u{1F1FF}]: This range matches emoji characters from the Regional Indicator Symbols block, which are used to represent country flags.
+[\u{1F900}-\u{1F9FF}]: This range matches emoji characters from the Supplemental Symbols and Pictographs block, which includes additional symbols and pictographs.
+[\u{1F170}-\u{1F251}]: This range matches emoji characters from the Enclosed Characters block, which includes characters enclosed in various shapes (such as circles and squares).
+[\u{1F600}-\u{1F636}]: This range matches emoji characters from the Emoticons block, which includes emoticons and smiley faces.
+[\u{1F681}-\u{1F6C5}]: This range matches emoji characters from the Additional Transport and Map Symbols block, which includes additional symbols related to transportation and maps.
+[\u{1F30D}-\u{1F567}]: This range matches emoji characters from the Miscellaneous Symbols and Arrows block, which includes various symbols and arrows.
+
+     [\u{1F300}-\u{1F64F}]:此范围匹配来自杂项符号和象形文字块的表情符号字符，其中包括各种符号和象形文字。
+     [\u{2702}-\u{27B0}]:此范围匹配Dingbats块中的表情符号字符，其中包括符号和图标。
+     [\u{1F680}-\u{1F6FF}]:此范围匹配来自交通和地图符号块的表情符号字符，其中包括与交通、地图和车辆相关的符号。
+     [\u{2600}-\u{26FF}]:此范围匹配杂项符号块中的表情符号字符，其中包括各种符号和图标。
+     [\u{1F1E6}-\u{1F1FF}]:此范围与区域指示符号块中的表情符号字符相匹配，用于表示国旗。
+     [\u{1F900}-\u{1F9FF}]:此范围匹配来自补充符号和象形文字块的表情符号字符，其中包括额外的符号和象形文字。
+     [\u{1F170}-\u{1F251}]:此范围匹配封闭字符块中的表情符号字符，其中包括以各种形状(如圆形和正方形)封闭的字符。
+     [\u{1F600}-\u{1F636}]:此范围匹配emoticon块中的表情符号字符，其中包括表情符号和笑脸。
+     [\u{1F681}-\u{1F6C5}]:此范围匹配来自附加交通和地图符号块的表情符号字符，其中包括与交通和地图相关的附加符号。
+     [\u{1F30D}-\u{1F567}]:此范围匹配来自杂项符号和箭头块的表情符号字符，其中包括各种符号和箭头。
+    */
+    RegExp regex = RegExp(
+      r"""([\u{1F300}-\u{1F64F}] 
+      |[\u{2702}-\u{27B0}]
+      |[\u{1F680}-\u{1F6FF}]
+      |[\u{2600}-\u{26FF}]
+      |[\u{1F1E6}-\u{1F1FF}]
+      |[\u{1F900}-\u{1F9FF}]
+      |[\u{1F170}-\u{1F251}]
+      |[\u{1F600}-\u{1F636}]
+      |[\u{1F681}-\u{1F6C5}]
+      |[\u{1F30D}-\u{1F567}])""",
+      unicode: true,
+    );
+    return regex.hasMatch(text);
   }
 }
