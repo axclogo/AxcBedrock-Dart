51c6e454c667901da24c556ad0bf244ae7bfbb87
axclogo <axclogo@163.com>
2023-08-09 09:03:37 +0800

    feat：new function with map and string

--- lib/extension/dart_lib/map_ex.dart
+++ lib/extension/dart_lib/map_ex.dart
@@ -2,7 +2,7 @@ part of axc_bedrock;
 
 class AxcMapSpace<Key, Value> {
   AxcMapSpace(Map<Key, Value> map) {
-    //构造方法
+    // 构造方法
     this.base = map;
   }
   Map<Key, Value> base = {};
@@ -19,12 +19,39 @@ extension MapToTransform<Key, Value> on AxcMapSpace<Key, Value> {}
 
 /// 属性 & Api
 extension MapToApi<Key, Value> on AxcMapSpace<Key, Value> {
-  ///返回给定键值[key]对应的值，如果[key]不在，则返回' null '
+  /// 返回与给定[predicate]或数字匹配的条目数
+  int count([bool Function(MapEntry<Key, Value>)? predicate]) {
+    if (predicate == null) {
+      return base.length;
+    }
+    var count = 0;
+    final i = base.entries.iterator;
+    while (i.moveNext()) {
+      if (predicate(i.current)) {
+        count++;
+      }
+    }
+    return count;
+  }
+
+  /// 返回给定键值[key]对应的值，如果[key]不在，则返回' null '
   Value? valueFor(Key key) {
     if (base[key] != null) {
       return base[key];
     }
     return null;
+  }
+
+  /// 返回一个包含所有匹配给定[predicate]的键值对的新映射。
+  /// 返回的map表保留原map表的表项迭代顺序。
+  Map<Key, Value> filter(bool Function(MapEntry<Key, Value> entry) predicate) {
+    final result = <Key, Value>{};
+    for (final entry in base.entries) {
+      if (predicate(entry)) {
+        result[entry.key] = entry.value;
+      }
+    }
+    return result;
   }
 }
 
--- lib/extension/dart_lib/string_ex.dart
+++ lib/extension/dart_lib/string_ex.dart
@@ -264,29 +264,39 @@ extension StringToGuard on AxcStringSpace {
   }
 
   /// 判断是否包含Emoji表情
-  bool hasEmoji(String text) {
+  bool isContainEmoji(String text) {
     /**
-     [\u{1F300}-\u{1F64F}]: This range matches emoji characters from the Miscellaneous Symbols and Pictographs block, which includes various symbols and pictographs.
-[\u{2702}-\u{27B0}]: This range matches emoji characters from the Dingbats block, which includes symbols and icons.
-[\u{1F680}-\u{1F6FF}]: This range matches emoji characters from the Transport and Map Symbols block, which includes symbols related to transportation, maps, and vehicles.
-[\u{2600}-\u{26FF}]: This range matches emoji characters from the Miscellaneous Symbols block, which includes various symbols and icons.
-[\u{1F1E6}-\u{1F1FF}]: This range matches emoji characters from the Regional Indicator Symbols block, which are used to represent country flags.
-[\u{1F900}-\u{1F9FF}]: This range matches emoji characters from the Supplemental Symbols and Pictographs block, which includes additional symbols and pictographs.
-[\u{1F170}-\u{1F251}]: This range matches emoji characters from the Enclosed Characters block, which includes characters enclosed in various shapes (such as circles and squares).
-[\u{1F600}-\u{1F636}]: This range matches emoji characters from the Emoticons block, which includes emoticons and smiley faces.
-[\u{1F681}-\u{1F6C5}]: This range matches emoji characters from the Additional Transport and Map Symbols block, which includes additional symbols related to transportation and maps.
-[\u{1F30D}-\u{1F567}]: This range matches emoji characters from the Miscellaneous Symbols and Arrows block, which includes various symbols and arrows.
+     [\u{1F300}-\u{1F64F}]: 
+     from the Miscellaneous Symbols and Pictographs block, which includes various symbols and pictographs.
+     [\u{2702}-\u{27B0}]: 
+     from the Dingbats block, which includes symbols and icons.
+     [\u{1F680}-\u{1F6FF}]:
+     from the Transport and Map Symbols block, which includes symbols related to transportation, maps, and vehicles.
+     [\u{2600}-\u{26FF}]: 
+     from the Miscellaneous Symbols block, which includes various symbols and icons.
+     [\u{1F1E6}-\u{1F1FF}]: 
+     from the Regional Indicator Symbols block, which are used to represent country flags.
+     [\u{1F900}-\u{1F9FF}]: 
+     from the Supplemental Symbols and Pictographs block, which includes additional symbols and pictographs.
+     [\u{1F170}-\u{1F251}]: 
+     from the Enclosed Characters block, which includes characters enclosed in various shapes (such as circles and squares).
+     [\u{1F600}-\u{1F636}]: 
+     from the Emoticons block, which includes emoticons and smiley faces.
+     [\u{1F681}-\u{1F6C5}]: 
+     from the Additional Transport and Map Symbols block, which includes additional symbols related to transportation and maps.
+     [\u{1F30D}-\u{1F567}]: 
+     from the Miscellaneous Symbols and Arrows block, which includes various symbols and arrows.
 
-     [\u{1F300}-\u{1F64F}]:此范围匹配来自杂项符号和象形文字块的表情符号字符，其中包括各种符号和象形文字。
-     [\u{2702}-\u{27B0}]:此范围匹配Dingbats块中的表情符号字符，其中包括符号和图标。
-     [\u{1F680}-\u{1F6FF}]:此范围匹配来自交通和地图符号块的表情符号字符，其中包括与交通、地图和车辆相关的符号。
-     [\u{2600}-\u{26FF}]:此范围匹配杂项符号块中的表情符号字符，其中包括各种符号和图标。
-     [\u{1F1E6}-\u{1F1FF}]:此范围与区域指示符号块中的表情符号字符相匹配，用于表示国旗。
-     [\u{1F900}-\u{1F9FF}]:此范围匹配来自补充符号和象形文字块的表情符号字符，其中包括额外的符号和象形文字。
-     [\u{1F170}-\u{1F251}]:此范围匹配封闭字符块中的表情符号字符，其中包括以各种形状(如圆形和正方形)封闭的字符。
-     [\u{1F600}-\u{1F636}]:此范围匹配emoticon块中的表情符号字符，其中包括表情符号和笑脸。
-     [\u{1F681}-\u{1F6C5}]:此范围匹配来自附加交通和地图符号块的表情符号字符，其中包括与交通和地图相关的附加符号。
-     [\u{1F30D}-\u{1F567}]:此范围匹配来自杂项符号和箭头块的表情符号字符，其中包括各种符号和箭头。
+     [\u{1F300}-\u{1F64F}]: 杂项符号和象形文字块的表情符号字符，其中包括各种符号和象形文字。
+     [\u{2702}-\u{27B0}]: Dingbats块中的表情符号字符，其中包括符号和图标。
+     [\u{1F680}-\u{1F6FF}]: 交通和地图符号块的表情符号字符，其中包括与交通、地图和车辆相关的符号。
+     [\u{2600}-\u{26FF}]: 杂项符号块中的表情符号字符，其中包括各种符号和图标。
+     [\u{1F1E6}-\u{1F1FF}]: 与区域指示符号块中的表情符号字符相匹配，用于表示国旗。
+     [\u{1F900}-\u{1F9FF}]: 补充符号和象形文字块的表情符号字符，其中包括额外的符号和象形文字。
+     [\u{1F170}-\u{1F251}]: 封闭字符块中的表情符号字符，其中包括以各种形状(如圆形和正方形)封闭的字符。
+     [\u{1F600}-\u{1F636}]: emoticon块中的表情符号字符，其中包括表情符号和笑脸。
+     [\u{1F681}-\u{1F6C5}]: 附加交通和地图符号块的表情符号字符，其中包括与交通和地图相关的附加符号。
+     [\u{1F30D}-\u{1F567}]: 杂项符号和箭头块的表情符号字符，其中包括各种符号和箭头。
     */
     RegExp regex = RegExp(
       r"""([\u{1F300}-\u{1F64F}] 
